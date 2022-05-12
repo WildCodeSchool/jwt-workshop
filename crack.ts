35dafead2d9f04d4b7384c65d1685619e69a3621
Arnaud Renaud <arnaud.renaud@gmail.com>
2022-05-12 15:26:21 +0200

    Use term "candidate key" instead of "password"

--- crack.ts
+++ crack.ts
@@ -2,7 +2,9 @@ import * as fs from "fs";
 import HMACSHA256 from "crypto-js/hmac-sha256";
 import { enc } from "crypto-js";
 
-const passwords: string[] = JSON.parse(fs.readFileSync("passwords", "utf-8"));
+const candidateKeys: string[] = JSON.parse(
+  fs.readFileSync("candidateKeys", "utf-8")
+);
 
 let jwt: string = fs.readFileSync("jwt", "utf-8");
 
@@ -12,9 +14,9 @@ const originalHeaderAndPayload = jwt.split(".")[0] + "." + jwt.split(".")[1];
 
 const originalSignature = jwt.split(".")[2];
 
-for (let currentPassword of passwords) {
-  console.log(currentPassword);
-  const signature = HMACSHA256(originalHeaderAndPayload, currentPassword);
+for (let key of candidateKeys) {
+  console.log(key);
+  const signature = HMACSHA256(originalHeaderAndPayload, key);
   if (enc.Base64.stringify(signature) === originalSignature) {
     console.log("---------found------------");
     break;
